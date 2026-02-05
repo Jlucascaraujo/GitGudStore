68fa798fa3de97fbd4238b9ab471351eefdf0d26
jlucascaraujo <jlucascaraujo@gmail.com>
2026-02-05 11:26:48 -0300

    finalizando a ativ 1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Fontes
+1. https://getbootstrap.com/docs/3.4/css/
+
 # GitGudStore
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.1.1.
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Login } from './login/login';
+import { TestPage } from './test-page/test-page';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'login', component: Login},
+    { path: 'test-page', component: TestPage},
     { path: '**', redirectTo: ''}
 ];
